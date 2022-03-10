fa79c6118ccfaf9cefa0f24acb6315c87513dbc2
Aurelie Gedeon <agedeon540@gmail.com>
2022-03-10 16:40:19 -0500

    tried to change image as background image

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,24 @@
 import { AppBar, Box, Toolbar, Typography, Button } from "@mui/material";
 import MenuDrawer from "./MenuDrawer";
+import "../App.css";
 
 export default function Header() {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
+    <Box sx={{ flexGrow: 1 }} className="header">
+      <AppBar position="fixed">
+        <Toolbar style={{ backgroundColor: "#224907" }}>
           <MenuDrawer />
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1 }}
+            className="web-name"
+          >
             Plant Baby
           </Typography>
-          <Button color="inherit">Login</Button>
+          <Button color="inherit" className="login-button">
+            Login
+          </Button>
         </Toolbar>
       </AppBar>
     </Box>
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,13 +1,18 @@
-import HeroImage from "../assets/PlantBabyHeroImage.png";
+import HeroImage from "../assets/PlantBabyHeroImg.png";
+import "../components/Hero.css";
+import React from "react";
 
 export default function Hero() {
   return (
     <>
-      <img
-        src={HeroImage}
-        alt="a room with many indoor houseplants. Some are resting on a shelf; others are big enough to sit on the ground"
-        width="100%"
-      />
+      <div class="hero-image">
+        {/* <img
+          src={HeroImage}
+          alt="a room with many indoor houseplants. Some are resting on a shelf; others are big enough to sit on the ground"
+          width="100%"
+        /> */}
+        <button className="lol">Hello</button>
+      </div>
     </>
   );
 }
